509880bc6b47356b5d47dbcca12e703b6f84d54e
kingofmidgard <singhsatyaprakash70675@gmail.com>
2025-05-20 07:23:52 +0530

    dikstraAlgorithm

--- src/utils/dijkstra.js
+++ src/utils/dijkstra.js
@@ -1,20 +1,29 @@
 import PriorityQueue from './priorityQueue.js';
 
-// Dijkstra Algorithm (Generator)
-export function* dijkstraStepByStep(graph, startNodeId) {
-  const distances = {};
-  const path = {};
-  const visited = new Set();
+// Dijkstra Algorithm (Genearator)
+export function* dijkstraStepByStep(graph, startNodeId) { //star marks as genrator
+  const distances = {}; // intiailize (key,distance)
+  const path = {}; //(key,path)
+  const visited = new Set(); //to mark vistied or not...
   const pq = new PriorityQueue();
 
   graph.nodes.forEach((node) => {
-    distances[node.id] = Infinity;
-    path[node.id] = null;
+    distances[node.id] = Infinity; //set disatance all to infinity
+    path[node.id] = null;  //path null as it unknown initally
   });
-  distances[startNodeId] = 0;
-  pq.enqueue(startNodeId, 0);
 
-  yield { phase: 'init', distances: { ...distances }, visited: new Set(visited), path: { ...path }, queue: pq.getNodes() };
+  distances[startNodeId] = 0; //start node disatnce 0
+  pq.enqueue(startNodeId, 0); //push in pq
+
+  //**yield** is a keyword used inside generator functions to pause and resume execution.
+  yield { 
+    phase: 'init', 
+    distances: { ...distances }, 
+    visited: new Set(visited), 
+    path: { ...path }, 
+    queue: pq.getNodes() 
+  };
+
 
   while (!pq.isEmpty()) {
     const current = pq.dequeue();
@@ -22,31 +31,73 @@ export function* dijkstraStepByStep(graph, startNodeId) {
     const currentNodeId = current.nodeId;
     visited.add(currentNodeId);
 
-    yield { phase: 'visiting', currentNode: currentNodeId, distances: { ...distances }, visited: new Set(visited), path: { ...path }, queue: pq.getNodes() };
+    yield { 
+      phase: 'visiting', 
+      currentNode: currentNodeId, 
+      distances: { ...distances }, 
+      visited: new Set(visited), 
+      path: { ...path }, 
+      queue: pq.getNodes() 
+    };
 
-    const neighbors = graph.edges.filter((edge) => edge.from === currentNodeId || edge.to === currentNodeId);
+    const neighbors = graph.edges.filter((edge) => edge.from === currentNodeId || edge.to === currentNodeId); //removing currentnode...take all nodes undirected... from as well as to
 
     for (const edge of neighbors) {
       const neighborId = edge.from === currentNodeId ? edge.to : edge.from;
       if (!visited.has(neighborId)) {
-        yield { phase: 'updating', currentNode: currentNodeId, distances: { ...distances }, visited: new Set(visited), path: { ...path }, queue: pq.getNodes(), exploringEdge: edge };
+        yield { 
+          phase: 'updating', 
+          currentNode: currentNodeId, 
+          distances: { ...distances }, 
+          visited: new Set(visited), 
+          path: { ...path }, 
+          queue: pq.getNodes(), 
+          exploringEdge: edge 
+        };
 
         const newDist = distances[currentNodeId] + edge.weight;
+
         if (newDist < distances[neighborId]) {
           distances[neighborId] = newDist;
           path[neighborId] = currentNodeId;
+
           console.log('Updated path:', { neighborId, predecessor: currentNodeId });
-          if (pq.has(neighborId)) pq.decreasePriority(neighborId, newDist);
+
+          if (pq.has(neighborId)){ //if node is already in queue update the distance and hesfiy their...
+            pq.decreasePriority(neighborId, newDist);
+          } //if not new insert
           else pq.enqueue(neighborId, newDist);
 
-          yield { phase: 'updating', updatedNode: neighborId, currentNode: currentNodeId, distances: { ...distances }, visited: new Set(visited), path: { ...path }, queue: pq.getNodes(), exploringEdge: edge };
+          yield { 
+            phase: 'updating', 
+            updatedNode: neighborId, 
+            currentNode: currentNodeId, 
+            distances: { ...distances }, 
+            visited: new Set(visited), 
+            path: { ...path }, 
+            queue: pq.getNodes(), 
+            exploringEdge: edge };
         }
       }
     }
-    yield { phase: 'visiting', currentNode: currentNodeId, distances: { ...distances }, visited: new Set(visited), path: { ...path }, queue: pq.getNodes(), exploringEdge: undefined };
+    yield { 
+      phase: 'visiting', 
+      currentNode: currentNodeId, 
+      distances: { ...distances }, 
+      visited: new Set(visited), 
+      path: { ...path }, 
+      queue: pq.getNodes(), 
+      exploringEdge: undefined };
   }
-
-  const finalState = { phase: 'done', currentNode: null, distances: { ...distances }, visited: new Set(visited), path: { ...path }, queue: [] };
+  //pq end...
+  const finalState = { 
+    phase: 'done', 
+    currentNode: null, 
+    distances: { ...distances }, 
+    visited: new Set(visited), 
+    path: { ...path }, 
+    queue: [] 
+  };
   console.log('Dijkstra final state:', finalState);
   yield finalState;
   return finalState;
